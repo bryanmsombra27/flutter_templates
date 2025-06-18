81182a337d001c0fdc03535421a25c5552d50451
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-17 22:37:45 -0600

    working on card desing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:templates/models/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,92 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Text("INITIAL TEMPLATE"),
+      home: Scaffold(
+        drawer: Drawer(
+          child: ListView.builder(
+            itemCount: menuItems.length,
+            itemBuilder: (context, index) {
+              final item = menuItems[index];
+
+              return ListTile(
+                leading: Icon(item.icon),
+                title: Text(item.title),
+              );
+            },
+          ),
+        ),
+        appBar: AppBar(title: Text("Shadow Orderfix")),
+        body: _home(),
+      ),
+    );
+  }
+}
+
+class _home extends StatelessWidget {
+  const _home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        spacing: 12,
+        children: [
+          _Card(color: Colors.red.shade100, icon: Icons.auto_graph_rounded),
+        ],
+      ),
+    );
+  }
+}
+
+class _Card extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  const _Card({super.key, required this.color, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: color,
+      ),
+
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.red.shade200,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+
+                  child: Icon(icon, size: 20, color: Colors.white),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text("Ventas del dia"),
+                    subtitle: Text("\$0.00"),
+                  ),
+                ),
+              ],
+            ),
+            Text("Generar reporte Ventas"),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/models/menu.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Menu {
+  final String title;
+  final IconData icon;
+
+  Menu({required this.title, required this.icon});
+}
+
+final List<Menu> menuItems = [
+  Menu(title: "Inicio", icon: Icons.home),
+  Menu(title: "Ordenes de trabajo", icon: Icons.work_history),
+  Menu(title: "Ventas", icon: Icons.monetization_on),
+  Menu(title: "Inventario", icon: Icons.edit_document),
+  Menu(title: "Ingresos", icon: Icons.attach_money_outlined),
+  Menu(title: "Compras | Gastos", icon: Icons.shopping_cart),
+  Menu(title: "Mantenimientos", icon: Icons.wallet_giftcard),
+  Menu(title: "Reportes", icon: Icons.document_scanner),
+  Menu(title: "Configuración", icon: Icons.settings),
+  Menu(title: "Salir", icon: Icons.logout),
+];
