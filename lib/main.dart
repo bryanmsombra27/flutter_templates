16de874e000a09111441488fa1bdfc78c8f088db
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-27 11:32:33 -0600

    adding bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,39 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(actions: [Icon(Icons.notification_important)]),
+        bottomNavigationBar: _NavigationBottomBar(),
 
         body: SingleChildScrollView(child: Home()),
+      ),
+    );
+  }
+}
+
+class _NavigationBottomBar extends StatelessWidget {
+  const _NavigationBottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        spacing: 20,
+        children: [
+          Icon(Icons.home),
+          Icon(Icons.favorite),
+          Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            alignment: Alignment.center,
+
+            child: Center(child: Icon(Icons.shopping_cart)),
+          ),
+          Icon(Icons.bookmark),
+          Icon(Icons.menu),
+        ],
       ),
     );
   }
--- lib/widgets/home_food_campaign.dart
+++ lib/widgets/home_food_campaign.dart
@@ -22,77 +22,77 @@ class HomeFoodCampaign extends StatelessWidget {
             TextButton(onPressed: () {}, child: Text("Ver Todas")),
           ],
         ),
-        Card(
-          child: Row(
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: [_FoodCard(), _FoodCard(), _FoodCard()]),
+        ),
+      ],
+    );
+  }
+}
+
+class _FoodCard extends StatelessWidget {
+  const _FoodCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        spacing: 10,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: Image.asset("assets/hamburguesa.png", width: 120),
+          ),
+
+          Column(
             spacing: 10,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: Image.asset("assets/hamburguesa.png", width: 120),
+              Container(
+                width: 150,
+                child: Text(
+                  "Hamburguesa",
+
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
               ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+                child: SizedBox(
+                  width: 150,
+                  child: Text("hamburguesa con papas y refresco", maxLines: 2),
+                ),
+              ),
+              RatingBar(
+                itemSize: 20,
+                itemCount: 5,
+                initialRating: 2,
+                onRatingUpdate: (rating) {},
+                direction: Axis.horizontal,
+                ratingWidget: RatingWidget(
+                  full: Icon(Icons.star),
+                  half: Icon(Icons.star_half),
+                  empty: Icon(Icons.star_border),
+                ),
+              ),
+              SizedBox(
+                width: 150,
+                child: Row(
+                  // spacing: 100,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
 
-              Column(
-                spacing: 10,
-                children: [
-                  Container(
-                    width: 150,
-                    child: Text(
-                      "Hamburguesa",
-
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 5,
-                      vertical: 4,
-                    ),
-                    child: SizedBox(
-                      width: 150,
-                      child: Text(
-                        "hamburguesa con papas y refresco",
-                        maxLines: 2,
-                      ),
-                    ),
-                  ),
-                  RatingBar(
-                    itemSize: 20,
-                    itemCount: 5,
-                    initialRating: 2,
-                    onRatingUpdate: (rating) {},
-                    direction: Axis.horizontal,
-                    ratingWidget: RatingWidget(
-                      full: Icon(Icons.star),
-                      half: Icon(Icons.star_half),
-                      empty: Icon(Icons.star_border),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 150,
-                    child: Row(
-                      // spacing: 100,
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "\$ 5",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Icon(Icons.add),
-                      ],
-                    ),
-                  ),
-                ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("\$ 5", style: TextStyle(fontWeight: FontWeight.bold)),
+                    Icon(Icons.add),
+                  ],
+                ),
               ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
