7a9cd8dd8e35c41ff7b3d9a27c5deef13e07bd8c
bryanmsombra27 <bryanjm96@gmail.com>
2025-07-12 09:10:40 -0600

    rework on floating design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,35 +13,144 @@ class Home extends StatelessWidget {
     final TextTheme textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
-      body: SizedBox(
-        height: height,
-        child: Stack(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
             _Welcome(height: height, colors: colors, textTheme: textTheme),
 
             SizedBox(height: 20),
-            _floatingDetails(
-              height: height,
-              width: width,
-              textTheme: textTheme,
-            ),
-            Positioned(
-              height: (height / 4 - 85) * 6,
 
-              child: Container(
-                padding: EdgeInsets.all(20),
-                width: width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Your Card", style: textTheme.titleLarge),
-                    TextButton(onPressed: () {}, child: Text("See all")),
-                  ],
-                ),
+            Transform.translate(
+              offset: Offset(0, -60),
+              transformHitTests: true,
+              child: Column(
+                spacing: 10,
+                children: [
+                  _floatingDetails(
+                    height: height,
+                    width: width,
+                    textTheme: textTheme,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    width: width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Your Card", style: textTheme.titleLarge),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "See all",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  _CreditCard(width: width),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Send Again",
+                          style: textTheme.titleLarge,
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          maxRadius: 30,
+                          backgroundColor: Colors.blue,
+                          child: Icon(Icons.add, color: Colors.white, size: 30),
+                        ),
+                        CircleAvatar(
+                          maxRadius: 30,
+
+                          backgroundImage: AssetImage("assets/ceo-avatar.jpeg"),
+                        ),
+                        CircleAvatar(
+                          maxRadius: 30,
+
+                          backgroundImage: AssetImage("assets/ceo-avatar.jpeg"),
+                        ),
+                        CircleAvatar(
+                          maxRadius: 30,
+
+                          backgroundImage: AssetImage("assets/ceo-avatar.jpeg"),
+                        ),
+                        CircleAvatar(
+                          maxRadius: 30,
+
+                          backgroundImage: AssetImage("assets/ceo-avatar.jpeg"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CreditCard extends StatelessWidget {
+  const _CreditCard({super.key, required this.width});
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: width * .9,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.credit_card),
+              Text(
+                "VISA",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+
+          Text("Card Number"),
+          Text(
+            "2148 3214 9812 2687",
+            style: TextStyle(fontWeight: FontWeight.bold, wordSpacing: 5),
+          ),
+
+          SizedBox(height: 20),
+          Text("Jhon Doe"),
+        ],
       ),
     );
   }
@@ -61,39 +170,35 @@ class _floatingDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: height / 4 - 80,
-      left: width / 20,
-      child: Container(
-        padding: EdgeInsets.all(15),
-        width: width * .9,
-        decoration: BoxDecoration(
-          color: Colors.blueGrey.shade100,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          spacing: 10,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Your balance"),
-            Text("\$70,501,00", style: textTheme.titleLarge),
-            SizedBox(height: 5),
+    return Container(
+      padding: EdgeInsets.all(15),
+      width: width * .9,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade100,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        spacing: 10,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Your balance"),
+          Text("\$70,501,00", style: textTheme.titleLarge),
+          SizedBox(height: 5),
 
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                spacing: 10,
-                children: [
-                  _IconBank(title: "Transfer"),
-                  _IconBank(title: "Request"),
-                  _IconBank(title: "Top Up"),
-                  _IconBank(title: "History"),
-                ],
-              ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              spacing: 10,
+              children: [
+                _IconBank(title: "Transfer"),
+                _IconBank(title: "Request"),
+                _IconBank(title: "Top Up"),
+                _IconBank(title: "History"),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
