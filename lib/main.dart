250514fa5804f6a30ada364cc70c8ea853b739a8
bryanmsombra27 <bryanjm96@gmail.com>
2025-07-22 12:20:12 -0600

    changing app name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shadow Delivery History',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
