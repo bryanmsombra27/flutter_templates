7e1eb82d83bbd60bba446bdfccc8d8d5d8ab012d
bryanmsombra27 <bryanjm96@gmail.com>
2025-07-27 12:47:01 -0600

    wrking on card container

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,66 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Text("INITIAL TEMPLATE"),
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  height: MediaQuery.of(context).size.height * .3,
+                  width: MediaQuery.of(context).size.width * .9,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(20),
+                        offset: Offset(0, 10),
+                        blurRadius: 10,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -20,
+                        right: -30,
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(child: Text("¡Estacionate!")),
+                        ),
+                      ),
+
+                      Row(
+                        spacing: 20,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(Icons.warehouse, size: 30),
+                          Column(
+                            children: [
+                              Text("Lugares disponibles"),
+                              Text("250"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
